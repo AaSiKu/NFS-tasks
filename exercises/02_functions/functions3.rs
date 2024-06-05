dbb2c1a1f7e987e16185c342991ab459793f60ba
AaSiKu <aasikucoding@gmail.com>
2024-06-05 23:51:41 +0530

    Reuploaded excercices till options

---
+++ exercises/02_functions/functions3.rs
@@ -0,0 +1,14 @@
+// functions3.rs
+//
+// Execute `rustlings hint functions3` or use the `hint` watch subcommand for a
+// hint.
+
+
+fn main() {
+    call_me(4);
+}
+fn call_me(num:u8) { 
+    for i in 0..num {
+        println!("Ring! Call number {}", i  +1);
+    }
+}
